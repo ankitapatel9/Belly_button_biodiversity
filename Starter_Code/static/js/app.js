0bb82f1530decb169d8cac451b144459f25d0a79
Anki Patel <batuk.girl91@gmail.com>
2019-12-10 00:03:41 -0600

    final changes in app.js

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -6,11 +6,11 @@ function optionChanged(value) {
     refreshDemographics(demographics);
 
     var chartData = window.rawData.samples.find(data => data.id === value);
-    console.log(chartData);
+    
 
     drawBarChart(chartData);
     drawBubbleChart(chartData);
-    builtPlot(demographics);
+   
 }
 
 function refreshDemographics(demographics) {
@@ -24,10 +24,10 @@ function refreshDemographics(demographics) {
 }
 
 
-function drawBarChart(chartData){    
-    var xData = chartData.sample_values.slice(0,10).reverse();
-    var barLabels = chartData.otu_labels.slice(0,10).reverse();
-    var yData = chartData.otu_ids.map(row => `OTU ${row}`).slice(0,10).reverse();
+function drawBarChart(chartData) {
+    var xData = chartData.sample_values.slice(0, 10).reverse();
+    var barLabels = chartData.otu_labels.slice(0, 10).reverse();
+    var yData = chartData.otu_ids.map(row => `OTU ${row}`).slice(0, 10).reverse();
 
     var trace = {
         type: "bar",
@@ -39,21 +39,24 @@ function drawBarChart(chartData){
     var data = [trace];
 
     //TODO: Remove top padding
-    Plotly.newPlot("bar", data, {}, {displayModeBar: false});
+    Plotly.newPlot("bar", data, {}, {
+        displayModeBar: false
+    });
 }
 
-function drawBubbleChart(chartData){
+function drawBubbleChart(chartData) {
     var xData = chartData.otu_ids;
     var yData = chartData.sample_values;
     var barLabels = chartData.otu_labels;
-    
+
     var trace2 = {
         x: xData,
         y: yData,
         mode: 'markers',
         marker: {
             color: xData,
-            size: yData
+            size: yData,
+            colorscale: "Earth"
         },
         text: barLabels
     };
@@ -63,52 +66,31 @@ function drawBubbleChart(chartData){
     var layout = {
         title: 'Marker Size',
         showlegend: false,
-        height:500,
+        height: 500,
         width: 1100
     };
     Plotly.newPlot('bubble', data, layout);
 }
 
-function addValue() {
+function init() {
     var url = "./samples.json";
     d3.json(url).then(function (data) {
-        console.log(data);
         window.rawData = data;
 
         // Add default option called Select in the dropdown
         var select = d3.select("#test-subject-dropdown");
         options = select.selectAll('option')
             .data(data.names)
-            .enter().append("option")
+            .enter()
+            .append("option")
             .attr("value", value => value)
             .text(text => text);
+
+        if(data.names.length > 0)
+        {
+            optionChanged(data.names[0]);
+        }
     });
 }
 
-addValue();
-
-function builtPlot(demographics) {
-    var value = demographics.wfreq;
-     var data = [{
-        domain: {
-            x: [0, 9],
-            y: [0, 9]
-        },
-        value: value,
-        title: {
-            text: "Speed"
-        },
-        type: "indicator",
-        mode: "gauge+number"
-    }];
-
-    var layout = {
-        width: 600,
-        height: 500,
-        margin: {
-            t: 0,
-            b: 0
-        }
-    };
-    Plotly.newPlot('gauge', data, layout);
-}
+init();
